d3abc6788bed1745e9b33ca5bd38602a8baacf04
Sampson0791 <sampson0791@gmail.comn>
2019-03-18 09:55:42 -0600

    created backend with get, post, delete routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./Routes/api/Items');
+
 const app = express();
 
 // body-parser middleware
@@ -15,6 +17,9 @@ mongoose
     .connect(db)
     .then(() => console.log('connected to mongo db'))
     .catch(err => console.log(err))
+
+// Require routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 3000;
 
