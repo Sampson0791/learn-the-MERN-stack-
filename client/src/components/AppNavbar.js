8e745150a80518899b8a99a0791630ae56a5f26b
Sampson0791 <sampson0791@gmail.comn>
2019-03-19 15:51:28 -0600

    Added shoppingList component to add and subtract items, using auto generated id's

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,47 +1,47 @@
 import React, { Component } from 'react';
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	Container
 } from 'reactstrap';
 
 class AppNavBar extends Component {
-    state = {
-        isOpen: false
-    }
-    
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+	state = {
+		isOpen: false
+	}
+	
+	toggle = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
 
-    render() {
-        return(
-            <div>
-                <Navbar color="dark" dark expand="sm" className="mb-5">
-                    <Container>
-                        <NavbarBrand href="/"> ShoppingList</NavbarBrand>
-                        <NavbarToggler onClick={this.toggle} />
-                        <Collapse isOpen={this.state.isOpen} navbar>
-                            <Nav className="ml-auto" navbar>
-                                <NavItem>
-                                    <NavLink href="https://github.com/Sampson0791/learn-the-MERN-stack-">
-                                        GitHub
-                                    </NavLink>  
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        );
-    }
+	render() {
+		return(
+			<div>
+				<Navbar color="dark" dark expand="sm" className="mb-5">
+					<Container>
+						<NavbarBrand href="/"> ShoppingList</NavbarBrand>
+						<NavbarToggler onClick={this.toggle} />
+						<Collapse isOpen={this.state.isOpen} navbar>
+							<Nav className="ml-auto" navbar>
+								<NavItem>
+									<NavLink href="https://github.com/Sampson0791/learn-the-MERN-stack-">
+										GitHub
+									</NavLink>  
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</Container>
+				</Navbar>
+			</div>
+		);
+	}
 }
 
 
